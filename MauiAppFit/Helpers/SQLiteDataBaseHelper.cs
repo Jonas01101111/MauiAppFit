2f6beeedf24c9335d8a32f1d9dbbb8103cc3530f
Jonas01101111 <jonas1kiubi@gmail.com>
2025-07-28 10:52:48 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppFit.Models;
+using SQLite;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    { 
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+
+        public Task<List<Atividade>>
+
+        
+    }
+}
